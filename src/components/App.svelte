1e0c4d6f3cfe12360869237e96a1ff7c95e454ac
Qcallahan <qcallahan@ucsd.edu>
2024-03-01 18:17:35 -0800

    basic banner

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,11 +3,25 @@
 </script>
 
 <main>
-  <h1>Svelte template</h1>
-
-  <p>Write your HTML here</p>
+  <div class="banner">
+    <h2>Why Dating Apps Suck</h2>
+  </div>
 </main>
 
 <style>
   /* Write your CSS here */
+  .banner {
+    background-color: #A74B62; /* Red color */
+    color: #FFE1E8; /* White text color */
+    padding: 20px;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    border-radius: 8px;
+    margin-bottom: 20px;
+  }
+
+  .banner h2 {
+    font-size: 24px;
+    margin: 0;
+  }
 </style>
